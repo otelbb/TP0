55e94aeef9d876d01ef41f6234e43a8c0b5a401c
otelbb <othmane.el-benaissi@polymtl.ca>
2024-09-04 16:00:35 -0400

    derniere verion

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
 import math
-#TODO Affichez l'aire d'un cercle avec le rayon indiqué
+#TODO  Affichez l'aire d'un cercle avec le rayon indiqué
 rayon = 4
-
+print("L'air du cercle est:" , rayon**2*3.14)
 #TODO Ajoutez une ligne qui affichera votre nom et prénom à la fin
+print("OTHMANE EL BENAISSI")
